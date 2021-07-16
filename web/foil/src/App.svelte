71006225230392dd20187746c38fc06e8c51a8fb
Jan Kuri <jkuri88@gmail.com>
2021-07-16 23:45:21 +0200

    chore(): unit web ui project

---
+++ web/foil/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	export let name;
+
+  fetch('/api/v1/streams')
+    .then(res => res.json)
+    .then(res => {
+      console.log(res);
+    })
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
